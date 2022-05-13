3a9b92b893fb9a71d4ae9ade2d7911988e66df68
Mihail <trotm96@mail.ru>
2022-05-13 12:09:40 +0300

    add test2

--- src/main/java/com/example/beerreview/mapper/PostMapper.java
+++ src/main/java/com/example/beerreview/mapper/PostMapper.java
@@ -4,9 +4,10 @@ package com.example.beerreview.mapper;
 import com.example.beerreview.dto.PostDTO;
 import com.example.beerreview.entity.Post;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
+@Component
 @Mapper(componentModel = "spring")
 public interface PostMapper {
-    PostDTO postToPostDTO(Post post);
     Post postDTOToPost(PostDTO postDTO);
 }

9e362340845f1d734af6da5658f2945fd8eac028
Mihail <trotm96@mail.ru>
2022-05-13 12:50:37 +0300

    add Dockerfil

--- src/main/java/com/example/beerreview/BeerReviewApplication.java
+++ src/main/java/com/example/beerreview/BeerReviewApplication.java
@@ -2,8 +2,10 @@ package com.example.beerreview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BeerReviewApplication {
 
     public static void main(String[] args) {
